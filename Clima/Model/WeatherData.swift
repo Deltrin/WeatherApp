8e2546f8575b6c6bef92fe269834f06b17b80bd2
Deltrin <deltrinsam@gmail.com>
2022-04-17 01:17:36 +0530

    first commit

---
+++ Clima/Model/WeatherData.swift
@@ -0,0 +1,9 @@
+//
+//  WeatherData.swift
+//  Clima
+//
+//  Created by Deltrin Sam on 14/03/22.
+//  Copyright © 2022 App Brewery. All rights reserved.
+//
+
+import Foundation
---
+++ Clima/Model/WeatherModel.swift
@@ -0,0 +1,9 @@
+//
+//  WeatherModel.swift
+//  Clima
+//
+//  Created by Deltrin Sam on 16/03/22.
+//  Copyright © 2022 App Brewery. All rights reserved.
+//
+
+import Foundation
